91b3cc2bbae6930fcd6c9954b34aae7790290910
mhali922 <56911307+mhali922@users.noreply.github.com>
2021-02-07 15:32:25 +0530

    Update DemoApp.java

--- src/main/java/com/mt/springmongo/DemoApp.java
+++ src/main/java/com/mt/springmongo/DemoApp.java
@@ -1,21 +1,22 @@
-package hello;
+package com.mt.springmongo;;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class Application {
+public class DemoApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApp.class, args);
+    }
 
     @RequestMapping("/")
     public String home() {
         return "Hello World - in Docker Container";
     }
 
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
-
-}
+ }
